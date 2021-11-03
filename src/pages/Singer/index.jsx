4606fe725fbe04ff85babee99eeae6a4ec835bb7
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-11-03 13:54:50 +0000

    Update songs

--- src/pages/Singer/index.jsx
+++ src/pages/Singer/index.jsx
@@ -7,8 +7,8 @@ import album3 from '../../images/2021-album.jpg';
 export const Singer = ({ songTitle }) => {
     const [albums] = useState([
         { id: 1, album: 'Up Next Session: Billie Eilish', releaseDate: '2017', songsArray: ['Ocean Eyes', 'Bellyache', 'Watch'], img: album1 },
-        { id: 2, album: 'When We All Fall Asleep, Where Do We Go?', releaseDate: '2019', songsArray: ['Bad Guy', 'Bury a Friend', 'When the party\'s over'], img: album2 },
-        { id: 3, album: 'Happier Than Ever', releaseDate: '2021', songsArray: ['Getting Older', 'I Didn\'t Change My Number', 'Billie Bossa Nova'], img: album3 },
+        { id: 2, album: 'When We All Fall Asleep, Where Do We Go?', releaseDate: '2019', songsArray: ['Bad Guy', 'Bury a Friend', 'When the party\'s over', 'Goodbye'], img: album2 },
+        { id: 3, album: 'Happier Than Ever', releaseDate: '2021', songsArray: ['My Future', 'Oxytocin', 'Lost Cause'], img: album3 },
     ])
 
     return (
